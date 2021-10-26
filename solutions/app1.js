0811a28c8bade5cb522463a5ec2e7ecdb6426f5f
Evelin <ladyblue@freemail.hu>
2021-10-26 13:57:41 +0200

    incorrect solution

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,10 +1,3 @@
-const setCookie = (value, name = 'token') => {
-    const now = new Date();
-    now.setTime(now.getTime() + (1000 * 60 * 15));
-    const expires = now.toUTCString();
-    // expiration dateTime
-    document.cookie = `${name}=${value};expires=${expires};path=/`;
-};
 
 export default setCookie;
 
